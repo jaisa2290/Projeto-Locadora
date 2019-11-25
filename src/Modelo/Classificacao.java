c257ba7feb2c3db25f93e1b7346987dcaa6620b4
MONIQUE BENTO <MONIQUE BENTO@LAPTOP-6MIBLN0H>
2019-11-25 14:13:38 -0300

    fiz telas do pacote Visao.Cadastrar

--- src/Modelo/Classificacao.java
+++ src/Modelo/Classificacao.java
@@ -3,15 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Principal;
- 
+package Modelo;
+
 /**
  *
  * @author MONIQUE BENTO
  */
-public class Main {
-  public static void main(String[] args) {  
-      new Login().setVisible(true);
-           
-  }
+public class Classificacao {
+    
 }
