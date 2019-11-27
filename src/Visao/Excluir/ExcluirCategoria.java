be5333d2de3119b63b0e81f022aefc836f8bdf90
MONIQUE BENTO <MONIQUE BENTO@LAPTOP-6MIBLN0H>
2019-11-27 08:12:43 -0300

    Alteração no excluir categoria

--- src/Visao/Excluir/ExcluirCategoria.java
+++ src/Visao/Excluir/ExcluirCategoria.java
@@ -39,7 +39,7 @@ public class ExcluirCategoria extends javax.swing.JFrame {
 
         jLabel2.setText("Nome:");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1" }));
 
         jButton1.setText("Ok");
 
