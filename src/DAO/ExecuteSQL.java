19b1acaf0dff4b926ebd7d12d5ae2a5cb46e9d87
MONIQUE BENTO <MONIQUE BENTO@LAPTOP-6MIBLN0H>
2019-12-04 07:54:20 -0300

    fiz o DAO

--- src/DAO/ExecuteSQL.java
+++ src/DAO/ExecuteSQL.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
-/**
- *
- * @author MONIQUE BENTO
- */
+import java.sql.Connection;
+
 public class ExecuteSQL {
     
+    private Connection con;
+    
+    public ExecuteSQL(Connection con) {
+        setCon(con);
+    }
+    
+    public Connection getCon() {
+        return con;
+    }
+     
+    public void setCon(Connection con) {
+        this.con = con;
+    }    
+       
 }
